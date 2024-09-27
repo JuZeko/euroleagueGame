a7a238621527c197afae0837721f402d3bb556e7
Ignobernvakaris <mantas.rutkauskas.vu@gmail.com>
2024-09-27 13:57:24 +0300

    added new header

--- euro-front/src/App.js
+++ euro-front/src/App.js
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from './components/header'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <button type="button" class="btn btn-danger">Danger</button>
-      </header>
+     <Header />
     </div>
   );
 }
