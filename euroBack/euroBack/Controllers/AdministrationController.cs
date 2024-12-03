5e8fe4552dc33d5daa9ee8cf2b3f5f4fddfe8ef9
JuZeko <realfanas@gmail.com.com>
2024-12-03 12:49:44 +0200

    Added DB and user log in

---
+++ euroBack/euroBack/Controllers/AdministrationController.cs
@@ -0,0 +1,21 @@
+﻿using euroBack.Database;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace euroBack.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AdministrationController(UserManager<User> userManager) : ControllerBase
+    {
+        private readonly UserManager<User> _userManager = userManager;
+
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+    }
+}
--- euroBack/euroBack/Controllers/IdenityController.cs
+++ euroBack/euroBack/Controllers/IdenityController.cs
@@ -6,6 +6,7 @@ namespace euroBack.Controllers
     {
         public IdenityController()
         {
+
         }
-     }
+    }
 }
