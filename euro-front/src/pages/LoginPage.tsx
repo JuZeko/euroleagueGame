20ae62b5d8b25525cc8a3140fb4c9ce7fe83806a
JuZeko <realfanas@gmail.com.com>
2024-12-03 18:00:42 +0200

    more changes

--- euro-front/src/pages/LoginPage.tsx
+++ euro-front/src/pages/LoginPage.tsx
@@ -1,75 +1,29 @@
-import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useNavigate } from "react-router-dom";
+import LoginForm from "src/components/LoginForm";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const loginData = {
-    email: email,
-    password: password,
-  };
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    navigate("/mainPage");
-  };
-  
   return (
-    <div className="container-fluid text-center" style={{  height: "100vh" }}>
-      <div className="row h-100" style={{flex: 1 }}>
-        <div className="col-6 d-flex flex-column" style={{border: "1rem solid",flex: 1 }}>
-          <div className="Jokic" style={{backgroundColor:"	#D8D8D8", flex: 1 }}>
-          </div>
-          <div className="Kobe" style={{border: "1rem solid", flex: 1 }}>
-          </div>
-          <div className="LebronDunk" style={{ flex: 1 }}>
-          </div>
+    <div className="container-fluid text-center" style={{ height: "100vh" }}>
+      <div className="row h-100" style={{ flex: 1 }}>
+        <div className="col-6 d-flex flex-column" style={{ flex: 1 }}>
+          <div
+            className="Jokic"
+            style={{ backgroundColor: "	#D8D8D8", flex: 1 }}
+          ></div>
+          <div className="Kobe" style={{ flex: 1 }}></div>
+          <div className="LebronDunk" style={{ flex: 1 }}></div>
         </div>
-        <div
-          className="col-6 d-flex flex-column"
-          style={{  border: "1rem solid",  flex: 1 }}
-        >
+        <div className="col-6 d-flex flex-column" style={{ flex: 1 }}>
           <div className="d-flex flex-fill justify-content-center align-items-center">
             <div className="card p-4" style={{ width: "20vw" }}>
               <h3 className="text-center mb-4">Sveiki</h3>
-              <form onSubmit={handleSubmit}>
-                <div className="form-group mb-3">
-                  <label htmlFor="email">Vartotojo vardas</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="username"
-                    placeholder="Vartotojo vardas"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="form-group mb-3">
-                  <label htmlFor="password">Slaptažodis</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    placeholder="Slaptažodis"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                </div>
-                <button type="submit" className="btn btn-primary w-100">
-                  Login
-                </button>
-              </form>
+              <LoginForm />
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}  
+};
 
 export default Login;
