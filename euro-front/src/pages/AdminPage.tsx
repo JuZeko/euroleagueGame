f3e39c66286e576d848ccb89ac8661b371db3791
JuZeko <realfanas@gmail.com.com>
2024-12-06 19:45:11 +0200

    Add tables

--- euro-front/src/pages/AdminPage.tsx
+++ euro-front/src/pages/AdminPage.tsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import CreateUserForm from "../components/UserForm";
+import UserTable from "src/components/UserTable";
+import UserCard from "src/components/UserCard";
+import TeamCard from "src/components/TeamCard";
 
 const AdminPage = () => {
-
   const [data, setData] = useState(null);
-  
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-       
-
-        
       } catch (error) {
-        console.error('Error fetching users:', error);
+        console.error("Error fetching users:", error);
       }
     };
 
@@ -24,24 +22,26 @@ const AdminPage = () => {
   return (
     <div>
       <Header />
-      <div className="container py-5" >
+      <div className="container pt-3">
         <div className="row">
           <div className="col-sm">
-            <CreateUserForm />
+            <UserCard />
           </div>
-         
+          <div className="col-sm">
+            <UserTable />
+          </div>
         </div>
         <div className="row">
           <div className="col-sm">
-            3
+            <TeamCard />
           </div>
-          <div className="col-sm">4</div>
+          <div className="col-sm">
+            <UserTable />
+          </div>
         </div>
       </div>
-      
     </div>
   );
 };
-
 
 export default AdminPage;
