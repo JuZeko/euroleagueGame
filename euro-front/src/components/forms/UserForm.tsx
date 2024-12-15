a90c4fb7eea1f296138c665e5e783e372fc23548
JuZeko <realfanas@gmail.com.com>
2024-12-15 14:51:08 +0200

    Create user

--- euro-front/src/components/forms/UserForm.tsx
+++ euro-front/src/components/forms/UserForm.tsx
@@ -1,9 +1,108 @@
-import React from "react";
+import React, { useState } from 'react';
+import { createUser } from 'src/services/AdministrationService';
+
+interface FormValues {
+  userName: string;
+  email: string;
+  password: string;
+  roleId: number;
+}
 
 const CreateUserForm: React.FC = () => {
+  const [formValues, setFormValues] = useState<FormValues>({
+    userName: '',
+    email: '',
+    password: '',
+    roleId: 1,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    const parsedValue = name === "roleId" ? parseInt(value) : value;
+    setFormValues({
+      ...formValues,
+      [name]: parsedValue,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const data = await createUser(formValues);  // Pass formValues to createUser
+      console.log('Created user:', data);
+    } catch (error) {
+     
+    }
+  };
+
   return (
     <div>
-      <h1>Hello</h1>
+      <form onSubmit={handleSubmit}>
+      <div>
+          <label htmlFor="username" className="form-label">
+            Vartotojo vardas:
+          </label>
+          <input
+            type="userName"
+            id="userName"
+            name="userName"
+            value={formValues.userName}
+            onChange={handleChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formValues.email}
+            onChange={handleChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formValues.password}
+            onChange={handleChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="roleId" className="form-label">
+            Role:
+          </label>
+          <select
+            id="roleId"
+            name="roleId"
+            value={formValues.roleId}
+            onChange={handleChange}
+            className="form-select"
+            required
+          >
+            {[0, 1].map((role) => (
+              <option key={role} value={role}>
+                Role {role}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sukurti
+        </button>
+      </form>
     </div>
   );
 };
---
+++ euroBack/euroBack/Dto/UserRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace euroBack.Dto
+{
+    public class UserRequestDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
